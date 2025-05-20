74c06dfe5441f352fd69364a075296a77252f085
Ricky Tang <rtang41@myseneca.ca>
2025-05-20 09:25:03 -0400

    added lab2e

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+##Author: Ricky Tang
+##Author ID: 104448246
+# Date Created: 2025/05/25
+
+timer = 10
+while timer > 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
