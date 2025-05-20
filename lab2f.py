0b24088dadb6b2392fbb227f124e2271eca2d471
Ricky Tang <rtang41@myseneca.ca>
2025-05-20 09:29:59 -0400

    added lab2f

---
+++ lab2f.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+##Author: Ricky Tang
+##Author ID: 104448246
+# Date Created: 2025/05/25
+import sys
+timer = int(sys.argv[1])
+while timer > 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
